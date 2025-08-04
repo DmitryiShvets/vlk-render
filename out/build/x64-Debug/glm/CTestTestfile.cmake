5c89289eea21bd1bd983f9cac47e1269f70c3504
DmitryiShvets <45293708+DmitryiShvets@users.noreply.github.com>
2025-08-04 12:44:31 +0300

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Dmitry/vlk_test/glm
-# Build directory: C:/Users/Dmitry/vlk_test/out/build/x64-Debug/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
